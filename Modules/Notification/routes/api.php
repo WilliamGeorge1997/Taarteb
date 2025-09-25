8cd2d3d888de2c6df87967287479d901fd2c27ea
William George <williamgeorge1997x@gmail.com>
2025-09-25 22:51:10 +0300

    update

--- Modules/Notification/routes/api.php
+++ Modules/Notification/routes/api.php
@@ -14,6 +14,11 @@ use Illuminate\Support\Facades\Route;
     |
 */
 
-Route::middleware(['auth:sanctum'])->prefix('v1')->name('api.')->group(function () {
-    Route::get('notification', fn (Request $request) => $request->user())->name('notification');
+Route::group([
+    'prefix' => 'notification',
+], function ($router) {
+
+    Route::get('all', 'NotificationController@index');
+    Route::post('read', 'NotificationController@readNotification');
+    Route::get('unReadNotificationsCount', 'NotificationController@unReadNotificationsCount');
 });
